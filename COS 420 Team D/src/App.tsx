bdf24988d4366a0ece27d30d4ebc46d86dd900cc
pstorer0219 <157522018+pstorer0219@users.noreply.github.com>
2025-03-27 13:34:16 -0400

    Added login/signup buttons

--- COS 420 Team D/src/App.tsx
+++ COS 420 Team D/src/App.tsx
@@ -2,18 +2,23 @@ import maineLogo from './assets/maine.jpg'
 import './App.css'
 
 function App() {
-
-
   return (
     <>
-      <div>
-        <a href="nothing" target="_blank">
-          <img src={maineLogo} className="logo Maine" alt="Maine logo" />
-        </a>
+      <div className="auth-buttons">
+        <a href="signup.html" className="auth-button signup">Sign Up</a>
+        <a href="login.html" className="auth-button login">Login</a>
       </div>
-      <h1>Umaine Hockey</h1>
-      <div className="card">
-          Click here to see the schedule!
+      
+      <div className="main-content">
+        <div>
+          <a href="nothing" target="_blank">
+            <img src={maineLogo} className="logo Maine" alt="Maine logo" />
+          </a>
+        </div>
+        <h1>UMaine Hockey</h1>
+        <div className="card">
+            Click here to see the schedule!
+        </div>
       </div>
     </>
   )
