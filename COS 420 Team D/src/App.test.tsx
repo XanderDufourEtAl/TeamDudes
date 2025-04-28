059ad85f22c229d7e4266f8613496ebddd80db9a
MatFri1 <matthew.frizzle@maine.edu>
2025-04-28 14:27:34 -0400

    Edit Test slightly

---
+++ COS 420 Team D/src/App.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('renders the homepage content', () => {
+    render(<App />);
+    expect(screen.getByText('UMaine Hockey')).toBeInTheDocument();
+    expect(screen.getByText('Click here for the Dashboard!')).toBeInTheDocument();
+   // expect(screen.getByText('Click here for the Baseball')).toBeInTheDocument();
+  });
+
+  it('shows navigation buttons', () => {
+    render(<App />);
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /login/i })).toBeInTheDocument();
+  });
+
+});
