3fdc48c0d0c2bc6ac7dec2a4ff5b677152a36a7f
Troy Schotter <43940407+TSchotter@users.noreply.github.com>
2025-04-30 00:24:25 -0400

    fixing the app page

--- COS 420 Team D/src/App.tsx
+++ COS 420 Team D/src/App.tsx
@@ -16,20 +16,6 @@ function App() {
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/widget" element={<WidgetPage />} />
-        {/* Main page with links */}
-        <Route path="/" element={
-          <div className="main-content">
-            <div>
-              <a href="nothing" target="_blank">
-                <img src={maineLogo} className="logo Maine" alt="Maine logo" />
-              </a>
-            </div>
-            <h1>UMaine Hockey</h1>
-            <div className="card">
-              <Link to="/widget" className="schedule-link"> Click here for the Dashboard!</Link>
-            </div>
-          </div>
-        } />
       </Routes>
     </Router>
   );
