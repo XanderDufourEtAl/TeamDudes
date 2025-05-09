b885e6a4eec742fb9ba4b39bbb6062fbae67e1df
RexTheMyth <rexthemyth@gmail.com>
2025-05-09 14:16:54 -0400

    Update CountDown.tsx

--- COS 420 Team D/src/CountDown.tsx
+++ COS 420 Team D/src/CountDown.tsx
@@ -9,56 +9,18 @@ import './CountDown.css';
 //const data = jsonData;
 console.log("test1");
 
-interface HockeyTeam {
-  teamId: number;
-  playerStats: PlayerStat[];
-  playerTotals: Totals;
-  totalStats: Totals;
-}
 
-interface PlayerStat {
-  firstName: string;
-  lastName: string;
-  name: string;
-  position: string;
-  goals: string;
-  assists: string;
-}
-
-interface TeamData {
-  shortName: string
-  id: string;
-}
-
-interface Totals {
-  goals: string;
-  assists: string;
-  shots: string;
-}
-
-interface Meta {
-  title: string;
-  description: string;
-  teams: TeamData[];
-}
-
-interface GameData {
-  meta: Meta;
-  teams: HockeyTeam[];
-}
-
-// Use the interface
-/*
-const gameData: GameData = jsonData;// Holds all the data from the json, accessed using the interfaces
-function Hockey(){
+//const gameData: GameData = jsonData;// Holds all the data from the json, accessed using the interfaces
+function CountDown(){
   //Have to use this to get the team names from the json as only team id and state name are given
+  /*
   const teamIdToShortName = gameData.meta.teams.reduce((map, team) => {
     map[team.id] = team.shortName;
     return map;
   }, {} as Record<string, string>);
   return (
     // Formatting the data to be displayed in the widget
-    <div className="hockey-container">
+    <div className="countdown-container">
       <h3>{gameData.meta.description}</h3>
       <div className="teams">
         <p>{teamIdToShortName[gameData.teams[0].teamId.toString()]} | {teamIdToShortName[gameData.teams[1].teamId.toString()]}</p>
@@ -66,6 +28,7 @@ function Hockey(){
       </div>
     </div>
   );
+  */
 }
-*/
-export default Hockey;
+
+export default CountDown;
